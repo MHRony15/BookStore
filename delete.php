53aa94fb84b60a065d02d71347afb7f1b9ee06a3
MHRony15 <37172763+MHRony15@users.noreply.github.com>
2022-01-06 19:21:16 +0600

    Add files via upload

--- delete.php
+++ delete.php
@@ -2,5 +2,5 @@
   $books = json_decode(file_get_contents('books.json'), true);
   array_splice($books, (int)$_GET['index'], 1);
   file_put_contents('books.json', json_encode($books));
-  echo 'Item Deleted. <a href="/BookStore/">Go Back</a>?';
+  echo 'Item Deleted. <a href="index.php">Go Back</a>?';
 ?>
--- search.php
+++ search.php
@@ -2,6 +2,6 @@
   $books = json_decode(file_get_contents('books.json'), true);
   $found = array_search($_GET['title'], array_column( $books, 'title' ) );
     if($found == true)
-      echo $_GET['title'].' is available. <a href="BookStore/">Borrow</a>?';
-    else echo $_GET['title'].' is not available. <a href="/BookStore/">Insert</a>?';
+      echo $_GET['title'].' is available. <a href="index.php">Borrow</a>?';
+    else echo $_GET['title'].' is not available. <a href="index.php">Insert</a>?';
 ?>
